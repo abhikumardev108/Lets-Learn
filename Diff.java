0d69611c0fb51f866bbadd43d53ed8e275c26c72
Abhishek Kumar <abhishekgupta121122@gmail.com>
2024-05-19 10:53:05 +0530

    added message in Diff.java

--- Diff.java
+++ Diff.java
@@ -2,6 +2,6 @@ public class Diff {
     public static void main(String[] args) {
         int a = 30;
         int b = 20;
-        System.out.println(a-b);
+        System.out.println("The diff is " + (a-b));
     }
 }
